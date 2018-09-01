ef4382866ad2419ef5fa643eac86ebba0e751035
rabex028 <rabex028@morris.umn.edu>
2018-08-31 23:00:50 -0500

    Add loop and conditional
    A loop and conditional statement for
    removing files which contain a particular
    string has been added to big_clean.sh, but is
    not quite working. big_clean.sh now creates a new
    directory to which tar files are extracted and uses
    variable names.

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+file="$1"
+directory="$2"
+here=$(pwd)
+mkdir $directory
+tar -xf $file -C $directory
+cd $directory
+for filename in $directory; do
+	if grep -Fxq "DELETE ME!" "$filename"; then
+		rm filename
+	fi
+done
 
-tar -xf $1 -C $2
+
 
